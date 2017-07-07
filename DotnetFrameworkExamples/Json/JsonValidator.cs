121d62d35c8ce6ef9ec58198d9330136034c0912
andvie <andvie@CA00200>
2017-07-07 12:29:12 +0200

    Added project; Added JsonValidator; Added JsonValidatorTests;

---
+++ DotnetFrameworkExamples/Json/JsonValidator.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Json
+{
+    public static class JsonValidator
+    {
+        public static bool IsValidJson(string s)
+        {
+            s = s.Trim();
+            
+            try
+            {
+                var obj = JToken.Parse(s);
+                return true;
+            }
+            catch (JsonReaderException jrex)
+            {
+                Console.WriteLine(jrex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }            
+            return false;            
+        }
+    }
+}
---
+++ DotnetFrameworkExamples/JsonTests/JsonValidatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Json;
+
+namespace JsonTests
+{
+    [TestClass]
+    public class JsonValidatorTests
+    {
+        static string _json = "{\"name\":\"test\"}";        
+
+        [TestMethod]
+        public void ShouldPass_IfValidJson()
+        {            
+            Assert.IsTrue(JsonValidator.IsValidJson(_json));
+        }
+
+        [TestMethod]
+        public void ShouldNotPass_IfJsonStringHasNoBeginningTag()
+        {
+            string _jsonNotValid = _json.Remove(0, 1);
+            Assert.IsFalse(JsonValidator.IsValidJson(_jsonNotValid));
+        }
+
+        [TestMethod]
+        public void ShouldNotPass_IfJsonStringBeginsWith2Tags()
+        {
+            string _jsonNotValid = "{" + _json;
+            Assert.IsFalse(JsonValidator.IsValidJson(_jsonNotValid));
+        }
+
+        [TestMethod]
+        public void ShouldNotPass_IfJsonStringEndsWith2Tags()
+        {
+            string _jsonNotValid = _json + "{";
+            Assert.IsFalse(JsonValidator.IsValidJson(_jsonNotValid));
+        }
+
+        [TestMethod]
+        public void ShouldNotPass_IfJsonStringEndsWithNoTag()
+        {
+            string _jsonNotValid = _json.Substring(_json.Length - 1);
+            Assert.IsFalse(JsonValidator.IsValidJson(_jsonNotValid));
+        }
+    }
+}
